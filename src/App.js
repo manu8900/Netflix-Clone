3a7114634da40c3574be692d345356f60fec51f7
manu8900 <gmanu19@gmail.com>
2020-12-03 22:03:27 +0530

    Made some changes and added images & files for app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Let's make netflix clone 🏳️‍🌈</h1>
+        <h1>Let's make netflix clone 📺 🏳️‍🌈</h1>
       </header>
     </div>
   );
