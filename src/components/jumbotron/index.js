d9602c3a68d8d968394b96f1f1559a37e58828a1
manu8900 <gmanu19@gmail.com>
2020-12-07 02:21:35 +0530

    Made some changes in Jumbotron Component

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Inner, Container } from "./styles/jumbotron";
+
+export default function Jumbotron({
+  children,
+  direction = "row",
+  ...restProps
+}) {
+  return (
+    <Inner direction={direction}>
+      {children}
+    </Inner>
+  );
+}
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
